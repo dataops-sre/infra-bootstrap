bb9618e18580b1b67d84c3e03496e14daa3906d8
zhaolong <zhaolong.liu@flixbus.com>
2020-11-28 17:19:27 +0100

    init commit

---
+++ infrastructure-live/dev/kubernetes/providers.tf
@@ -0,0 +1,42 @@
+variable "eks_cluster_endpoint" {
+  type        = string
+  default     = "dummy"
+  description = "describe your variable"
+}
+
+variable "eks_cluster_id" {
+  type        = string
+  default     = "dummy"
+  description = "describe your variable"
+}
+
+provider "aws" {
+  version = ">= 3.3.0"
+  region  = "eu-west-2"
+}
+
+provider "kubernetes" {
+  version                = "1.13.3"
+  load_config_file       = "false"
+  host                   = var.eks_cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.eks_cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.eks_cluster_id
+}
+
+provider "helm" {
+  version = "1.3.2"
+  kubernetes {
+    load_config_file       = "false"
+    host                   = var.eks_cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
